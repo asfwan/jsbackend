9d3b114a5b9625d06945ad44a6bc310b42a00392
asfwan <asfwan@gmail.com>
2021-09-19 17:41:47 +0800

    successful requests labeled

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,14 +2,17 @@ const helpers = require('../helpers');
 const models = require('../models/index');
 const User = models.User;
 
+/* Retrieve all users */
 const getAll = (req, res, next) => {
     User.findAll().then((users)=>{
         helpers.successResponse(res, users);
     }).catch((err)=>{
-        helpers.failedResponse(res, null, err.errors[0].message);
+        msg = err && err.errors && err.errors[0] ? err.errors[0].message : err;
+        helpers.failedResponse(res, null, msg);
     });
 }
 
+/* Retrieve all users */
 const createOne = (req, res, next) => {
     helpers.init({req, res, next});
     User.create(helpers.params).then((user)=>{
@@ -17,41 +20,46 @@ const createOne = (req, res, next) => {
             helpers.successResponse(res, user);
         })
     }).catch((err)=>{
-        helpers.failedResponse(res, null, err.errors[0].message);
+        msg = err && err.errors && err.errors[0] ? err.errors[0].message : err;
+        helpers.failedResponse(res, null, msg);
     });
 }
 
+/* Retrieve all users */
 const getOne = (req, res, next) => {
     User.findByPk(req.params.userId).then((user)=>{
         helpers.successResponse(res, user);
     }).catch((err)=>{
-        helpers.failedResponse(res, null, err.errors[0].message);
+        msg = err && err.errors && err.errors[0] ? err.errors[0].message : err;
+        helpers.failedResponse(res, null, msg);
     });
 }
 
 const updateOne = (req, res, next) => {
     User.update(req.params, {
         where:{
-            id: req.userId
+            id: req.params.userId
         }
     }).then((user)=>{
         User.findByPk(user.id).then((user)=>{
             helpers.successResponse(res, user);
         })
     }).catch((err)=>{
-        helpers.failedResponse(res, null, err.errors[0].message);
+        msg = err && err.errors && err.errors[0] ? err.errors[0].message : err;
+        helpers.failedResponse(res, null, msg);
     });
 }
 
 const deleteOne = (req, res, next) => {
-    User.delete(req.params, {
+    User.destroy(req.params, {
         where:{
-            id: req.userId
+            id: req.params.userId
         }
     }).then((user)=>{
         helpers.successResponse(res, user);
     }).catch((err)=>{
-        helpers.failedResponse(res, null, err.errors[0].message);
+        msg = err && err.errors && err.errors[0] ? err.errors[0].message : err;
+        helpers.failedResponse(res, null, msg);
     });
 }
 
