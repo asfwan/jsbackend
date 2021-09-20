f476f57c87ae579b74c1a303c3c62a8d1d44fbb6
asfwan <asfwan@gmail.com>
2021-09-20 13:22:38 +0800

    cleanup

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,6 @@
 var router = require('express').Router();
 var UserController = require('../controllers/user-controller');
 
-/* GET - retrieve users listing. */
-router.get('/', UserController.getAll);
-
-/* POST - create user. */
-router.post('/', UserController.createOne);
-
-/* GET - get user by id. */
-router.get('/:id', UserController.getOne);
-
-/* PUT - update user. */
-router.put('/:id', UserController.updateOne);
-
-/* DELETE - delete user. */
-router.delete('/:id', UserController.deleteOne);
+UserController.apiResources(router);
 
 module.exports = router;
