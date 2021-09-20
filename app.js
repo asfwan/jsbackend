3555e1551e03faab4e31c54ec983a8f41efa18c1
asfwan <asfwan@gmail.com>
2021-09-20 16:31:57 +0800

    root route is /api

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', apiRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
